5956fb91e0dbe051dd841cd5877f5cb4f27f38e6
Faizan Aalam <f24aalam@gmail.com>
2026-02-15 17:23:44 +0530

    fix: add postgres driver import in main.go

--- cmd/godbmcp/main.go
+++ cmd/godbmcp/main.go
@@ -1,12 +1,12 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
 import (
 	"github.com/f24aalam/godbmcp/cmd"
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	_ "modernc.org/sqlite"
 )
 
